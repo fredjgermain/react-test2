8decb33872d2a09360b694d7bf7bfd435c7576da
Fred <fredjgermain@gmail.com>
2020-12-09 16:54:49 -0500

    test admin section

--- src/App.js
+++ src/App.js
@@ -3,16 +3,13 @@ import React from 'react';
 /*import TestInput from './components/testinput/input/testInput'; 
 import TestInput2 from './components/testinput/input/testInput2'; */
 import AdminSection from './components/adminsection/adminsection'; 
-import {crud} from './mongoosedao/crudaxios';
+//import TestCors from './components/testcors';
 
 
 export default function App() { 
   //return <TestInput /> 
   //return <TestInput2 /> 
   //return <TestInputTable /> 
-  async function Access() { 
-    return await crud.Access(); 
-  } 
-  return Access(); 
-  //return <AdminSection />; 
+  //return <TestCors/>; 
+  return <AdminSection />; 
 } 
